8c2c092347037cb95790749533cf60b241800048
Taghreed Almutairi <taghreed12200@outlook.sa>
2020-11-21 21:41:22 +0300

    intial commit

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+$con=mysqli_connect("localhost","root","","login")
+    or die (mysqli_connect_error());
+
+    mysqli_query($con,"CREATE TABLE IF NOT EXISTS users(
+      id INT NOT NULL AUTO_INCREMENT,
+      PRIMARY KEY (id),
+      username VARCHAR(100),
+      passowrd VARCHAR(100))")
+      or die (mysqli_connect_error());
+
+
+?>
