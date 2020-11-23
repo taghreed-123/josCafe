9d3640c08741b9c18774209ff1b91efb4f9f4edd
Taghreed Almutairi <taghreed12200@outlook.sa>
2020-11-23 03:15:24 +0300

    intial commit

--- register.php
+++ register.php
@@ -4,17 +4,22 @@ session_start();
 $con = mysqli_connect('localhost','root','');
 mysqli_select_db($con,'userregistration');
 
-mysqli_query($con,"CREATE TABLE IF NOT EXISTS users(
-  id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id),
-  username VARCHAR(100),
-  passowrd VARCHAR(100))")
-  or die (mysql_connect_error());
-
 $name =$_POST['user'];
-$pass =$_POST['passowrd'];
+$pass =$_POST['password'];
 
 
 
+$s =" select *from usertable where name='$name'";
 
+$result = mysqli_query($con,$s);
+
+$num = mysqli_num_rows($result);
+
+if($num ==1){
+  echo "username Already Taken";
+}else{
+  $reg ="insert into usertable(name,passowrd) values ('$name','$pass')";
+  mysqli_query($con,$reg);
+  echo "Registration Successful";
+}
  ?>
