5033a04a8b99b1e64bf3d2e123841bb2696c79d6
Taghreed Almutairi <taghreed12200@outlook.sa>
2020-11-25 21:56:54 +0300

    intial commit

--- validation.php
+++ validation.php
@@ -6,17 +6,17 @@ $con = mysqli_connect('localhost','root','');
 mysqli_select_db($con,'userregistration');
 
   $name =$_POST['user'];
-  $pass =$_POST['passowrd'];
+  $pass =$_POST['password'];
 
   $s = "select * from users where name = '$name' && password = '$pass'";
 
-  $result = mysqli_query($con ,$c);
+  $result = mysqli_query($con,$s);
 
   $num = mysqli_num_rows($result);
-  if($num ==1){
-    header('location:home.html');
+  if($num==1){
+    header('location:home.php');
   }else{
-    header('location:login.php');
+    header('location:home.php');
   }
 
  ?>
