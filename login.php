03ec762a47759573b5c57e922e9808e30e3d317a
shomookh <shomookh1900@hotmail.com>
2020-11-28 00:44:46 +0300

    intial commit

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title> User Login and Registration</title>
+    <title> تسجيل </title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
