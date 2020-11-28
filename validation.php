f3ed7e275d353e3c28b51c39f0031a5e2a098203
montaha-alqahtani <74149138+montaha-alqahtani@users.noreply.github.com>
2020-11-28 16:51:14 +0300

    Update validation.php

--- validation.php
+++ validation.php
@@ -3,33 +3,21 @@ session_start();
 
 $con = mysqli_connect('localhost','root','');
 mysqli_select_db($con,'userregistration');
+if(isset($_POST['user'])&&isset($_POST['password'])){
 
   $name =$_POST['user'];
   $pass =$_POST['password'];
 
   $query = " SELECT*FROM usertable  WHERE name='$name' && password='$pass'";
+if(mysqli_num_rows(mysqli_query($con,$query))>0){
+	 $_SESSION['name']=$row['name'];
+   header("Location:home.php?=success");
+  
+}
+else{
+	echo"اسم المستخدم او كلمه المرور خطأه";
+}
 
-  $result=mysqli_query($con,$query);
-$resultCheck = mysqli_num_rows($result);
-
-
-if($resultCheck>0){
-  header("Location:login.php?erorr=sqlerror");
-  exit();
-}else {
-  while ($row=mysqli_fetch_assoc($result)) {
-    $conditional_pass = password_verify($pass, $row['password']);
-    if(!$conditional_pass){
-
-
-     header("Location:landing.php?error=$conditional_pass");
-     echo "string";
-     exit();
-  }
-    session_start();
-    $_SESSION['name']=$row['name'];
-    header("Location:home.php?=success");
-  }
 }
 
 
