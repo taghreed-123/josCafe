38ddcc93de4370bfe941944af7a8f859592b80a8
montaha-alqahtani <74149138+montaha-alqahtani@users.noreply.github.com>
2020-11-28 23:13:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-  alert(" اهلًا بك في جوس كافيه .. تفضل لتسجيل الدخول  ")
+   			  
+  
+            function validation()  
+            {  
+                var id=document.f1.user.value;  
+                var ps=document.f1.password.value;  
+                if(id.length=="" && ps.length=="") {  
+                    alert("User Name and Password fields are empty");  
+                    return false;  
+                }  
+                else  
+                {  
+                    if(id.length=="") {  
+                        alert("User Name is empty");  
+                        return false;  
+                    }   
+                    if (ps.length=="") {  
+                    alert("Password field is empty");  
+                    return false;  
+                    }  
+                }                             
+            }  
+    
